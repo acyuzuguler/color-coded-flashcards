2050bb2fe2a638d73b3a5adb988e15184ce0f2f3
Ahmet Yuzuguler <acyuzuguler@gmail.com>
2025-03-01 17:41:56 +0100

    Delete old files

--- generate_pdfs.py
+++
@@ -1,5 +0,0 @@
-import os 
-
-google_chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-
-os.system(f"{google_chrome} --headless --print-to-pdf=flashcards.pdf --no-margins flashcards.html")
